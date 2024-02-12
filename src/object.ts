7fc1a4565ec1d54c0041f8a5d5a824c60998a0ce
raihanuldev <rmmahi4770@gmail.com>
2024-02-12 11:41:05 +0600

    object type

---
+++ src/object.ts
@@ -0,0 +1,15 @@
+const user:{
+    address:"cox",
+    name:string,
+    age:number,
+    isMarried:boolean,
+    wife?:string,
+    child?:boolean
+} = {
+    name:"Hasem Ali",
+    address:"cox",
+    age:54,
+    isMarried:true,
+    wife:'Hasina',
+    child:false
+}
