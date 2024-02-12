43132fdf7331d0d4d07783623f1c1af91f6637db
raihanuldev <rmmahi4770@gmail.com>
2024-02-12 11:57:04 +0600

    funciton

---
+++ src/function.ts
@@ -0,0 +1,14 @@
+// normal function
+
+function add(num1:number,num2:number):number{
+    return num1+num2;
+}
+
+// Arrow Function
+const odd=(biggestNum:number,lowestNum:number):number=>{
+    return biggestNum-lowestNum;
+}
+
+const welcomeMessage =():void=>{
+    return console.log("Welcome to this documentation")
+}
