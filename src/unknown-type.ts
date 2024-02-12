864eea5bcd415e97142bea7ebbe68bf4ff2f7608
raihanuldev <rmmahi4770@gmail.com>
2024-02-13 00:02:56 +0600

    UNKNOWN TYPE

--- src/unknown-type.ts
+++ src/unknown-type.ts
@@ -1,18 +1,27 @@
 // unknown type
 
-const getCarSpeed =(speed:unknown)=>{
-    if(typeof speed==='number'){
-        const converted = speed*1000/3600;
+const getCarSpeed = (speed: unknown) => {
+    if (typeof speed === 'number') {
+        const converted = speed * 1000 / 3600;
         console.log(`Your Speed ${converted} Metar`)
     }
-    if(typeof speed === 'string'){
+    if (typeof speed === 'string') {
         const value = speed.split(" ")
-        const mainValue =parseFloat( value[0])
-        const converted = mainValue*1000/3600;
-        console.log(`Your Speed ${converted} Metar`)
+        console.log(value)
+        const mainValue = parseFloat(value[0])
+        if (typeof mainValue === 'number') {
+            const converted = mainValue * 1000 / 3600;
+            console.log(`Your Speed ${converted} Metar`)
+        }
+        else {
+            console.log("This is Nan")
+        }
 
+    }
+    else {
+        console.log("Something worng")
     }
 }
 
-getCarSpeed(154804394329042348848);
+getCarSpeed("eyqwu");
 getCarSpeed("1567 kmh^-1");
