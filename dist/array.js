27b01267c0ee96277db727c6d9de919c15e5b390
raihanuldev <rmmahi4770@gmail.com>
2024-02-12 11:25:29 +0600

    array & Tuple

---
+++ dist/array.js
@@ -0,0 +1,6 @@
+"use strict";
+const names = ['abul', 'kabol', 'hasem'];
+const rolls = [321, 3213, 312, 412341231241, 44142344213, 123, 41243213, 42134, 234];
+// Example Tuple
+const copule = ['kabila', 'kabil'];
+const copule1 = ['hasem', false];
---
+++ src/array.ts
@@ -0,0 +1,8 @@
+const names:string[] = ['abul', 'kabol', 'hasem']
+
+const rolls:number[] = [321,3213,312,412341231241,44142344213,123,41243213,42134,234]
+
+// Example Tuple
+const copule:[string,string] = ['kabila','kabil']
+
+const copule1: [string,boolean] =['hasem',false]
