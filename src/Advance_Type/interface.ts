968eff675156c951c4cba19964fb42197274444e
raihanuldev <rmmahi4770@gmail.com>
2024-02-14 02:03:39 +0600

    interface

---
+++ src/Advance_Type/interface.ts
@@ -0,0 +1,44 @@
+
+interface Habib {
+    name: string,
+    age: number
+}
+
+interface habibAndCurrentGf extends Habib {
+    bedirName:string,
+    bedirAge:number
+}
+
+const habibInfo :habibAndCurrentGf ={
+    name:'Habib',
+    age:45,
+    bedirAge:80,
+    bedirName:'Tasmin sultana'
+}
+
+// interface habibFristGfInterFaceType {
+//     gfFrist: string,
+//     fristGfAge: number,
+//     herEx: number
+// }
+
+// // cuurent gf info
+// interface habibSecondGfInterfaceType {
+//     gfSecound: string,
+//     secoundGfAge: number,
+//     herEx: number
+// }
+
+// Habib and Current gf
+
+
+
+
+// const habib:habibFristGfInterFaceType = {
+//     gfFrist:'Sondori',
+//     fristGfAge:45,
+//     herEx:12
+// }
+
+// Total Gf 
+
