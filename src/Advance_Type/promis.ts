0bba743cb8335c063d9d99e3d1bd0166ff6fe429
raihanuldev <rmmahi4770@gmail.com>
2024-02-15 15:25:07 +0600

    exmple of promis

---
+++ src/Advance_Type/promis.ts
@@ -0,0 +1,12 @@
+
+
+const makePromise = () => {
+    return new Promise((resolve, reject) => {
+        const info = "Succesfully fetch data";
+        if (info) {
+            resolve(info)
+        } else {
+            reject("Failed to Fetch Data")
+        }
+    })
+}
