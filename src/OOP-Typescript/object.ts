41495d688fb705e2a4a1fd3053a598815043edaf
raihanuldev <rmmahi4770@gmail.com>
2024-02-16 02:14:09 +0600

    object

---
+++ src/OOP-Typescript/object.ts
@@ -0,0 +1,21 @@
+// Exmaple Of Objeect
+
+class Animal {
+    name:string;
+    speciec:string;
+    sound:string;
+
+    constructor(name:string,speciec:string,sound:string){
+        this.name = name;
+        this.speciec = speciec;
+        this.sound = sound;
+    }
+    makeSound(){
+        console.log(`The ${this.name} says ${this.sound}`)
+    }
+}
+
+const dog = new Animal('henerita gomaje','dog','Ghew Ghew')
+const cat = new Animal('Hetanys ','cat',"mew mew")
+dog.makeSound();
+cat.makeSound()
